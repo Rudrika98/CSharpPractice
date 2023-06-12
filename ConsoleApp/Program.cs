1ecec845dd1113b413c593698d9ab5ac7d7f0a25
Rudrika <rudrikasharma94@gmail.com>
2023-06-12 13:33:32 +0530

    Practice project for C#

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 10;
+            int b = 39;
+            int c;
+            c = a + b;
+            Console.WriteLine("The sum of two numbers is: {0}", c);
+            Console.ReadLine();
+
+            Console.WriteLine("I love C#");
+
+        }
+    }
+}
